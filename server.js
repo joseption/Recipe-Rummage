74807ad9b81664866827821ddc520361eeb60e41
joseption <40270262+joseption@users.noreply.github.com>
2022-07-01 21:22:19 -0400

    conflicts

--- server.js
+++ server.js
@@ -17,17 +17,16 @@ const validatePassword = (password, passwordVerify) => {
   return symbol && upperCase && length && match;
 };
 
-require("dotenv").config({path:'./.env'});
+
 const { 
   v1: uuidv1,
   v4: uuid
 } = require('uuid');
 
 const MongoClient = require('mongodb').MongoClient;
-
 const {ObjectId} = require('mongodb');
 
-const {ObjectId} = require('mongodb');
+
 
 require('dotenv').config();
 const url = process.env.MONGODB_URI;
@@ -42,6 +41,7 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(cors());
 app.use(bodyParser.json());
+
 
 app.post('/api/login', async (req, res, next) => 
 {
