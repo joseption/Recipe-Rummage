3585117fdd4e93647f3c29666474bef694d2f80d
joseption <40270262+joseption@users.noreply.github.com>
2022-07-10 21:17:13 -0400

    fix for error message

--- frontend/src/pages/ProfilePage.js
+++ frontend/src/pages/ProfilePage.js
@@ -2,9 +2,11 @@ import '../styles/ProfilePage.css';
 import RecipeList from '../components/RecipeList';
 import GroceryList from '../components/GroceryList';
 import Navigation from '../components/Navigation';
+import { useState } from 'react';
 
 const ProfilePage = (props) =>
 {
+    const [recipeError,setRecipeError] = useState('');
     return(
         <div className="main-page-container">
             <Navigation mode={"profile"} />
@@ -14,7 +16,7 @@ const ProfilePage = (props) =>
                     <GroceryList mode={"profile"} />
                 </div>
                 <div className="recipe-list">
-                    <RecipeList searchPlaceHolder={"Find My Recipe"} title={"My Recipes"} mode={"profile"} />
+                    <RecipeList setRecipeError={setRecipeError} recipeError={recipeError} searchPlaceHolder={"Find My Recipe"} title={"My Recipes"} mode={"profile"} />
                 </div>
             </div>
         </div>
