c027c8e94fb4751603ffd5541f0cf78ec1c0cb1d
joseption <40270262+joseption@users.noreply.github.com>
2022-07-10 21:01:27 -0400

    Worked on recipe components

--- frontend/src/components/RecipeItem.js
+++ frontend/src/components/RecipeItem.js
@@ -2,56 +2,167 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import '../styles/RecipeItem.css';
 import { regular, solid } from '@fortawesome/fontawesome-svg-core/import.macro'
 import { useEffect, useState } from 'react';
-import { config } from '../Constants'
+import { Constant, config } from '../Constants'
+import { faPersonMilitaryToPerson } from '@fortawesome/free-solid-svg-icons';
 
 const RecipeItem = (props) =>
 {
+    const max_title = 300;
+    const max_desc = 500;
     var name; // input
     var description; // input
-    const [sName,setName] = useState(false);
-    const [sDesc,setDesc] = useState(false);
+    var card;
+    const [sName,setName] = useState('');
+    const [sDesc,setDesc] = useState('');
     const [isEditing,setIsEditing] = useState(false);
     const [isFavorite,setIsFavorite] = useState(false);
 
     useEffect(() => {
-        setName(props.item.name);
-        setDesc(props.item.description);
+        setName(props.item.name.substring(0, max_title));
+        setDesc(props.item.description.substring(0, max_desc) + "...");
         if (isEditing) {
             description.value = props.item.description;
             name.value = props.item.name;
         }
     }, [description, name, props.item, isEditing]);
 
+    const showEditor = () => {
+        setIsEditing(true);
+    }
+
     const cancelItem = (e) => {
         setIsEditing(false);
+        name.value = props.item.name;
+        description.value = props.item.description;
     };
 
     const removeItem = async (e) => {
-        
+        var cCard = card;
+        cCard.classList.remove("recipe-item-error");
+        let obj = {id:props.item._id};
+            let js = JSON.stringify(obj);
+            await fetch(`${config.URL}/api/remove-favorite`,
+            {method:'POST',body:js,headers:{'Content-Type': 'application/json', 'authorization': Constant.token}}).then(async ret => {
+                let res = JSON.parse(await ret.text());
+                if (res.error)
+                {
+                    cCard.classList.add("recipe-item-error");
+                }
+                else
+                {                       
+                    cCard.classList.remove("recipe-item-error");
+                    props.deleteItem();
+                }
+            }); 
     };
 
     const updateItem = async (e) => {
-        setIsEditing(false);
+        var cCard = card;
+        cCard.classList.remove("recipe-item-error");
+        name.classList.remove("input-error");
+        description.classList.remove("input-error");
+
+        let hasError = false;
+        if (!name.value) {
+            hasError = true;
+            name.classList.add("input-error");
+        }
+        if (!description.value) {
+            hasError = true;
+            description.classList.add("input-error");
+        }
+        if (hasError) {
+            return;
+        }
+
+        let obj = {id:props.item._id, name:name.value, description:description.value};
+            let js = JSON.stringify(obj);
+            await fetch(`${config.URL}/api/update-favorite`,
+            {method:'POST',body:js,headers:{'Content-Type': 'application/json', 'authorization': Constant.token}}).then(async ret => {
+                let res = JSON.parse(await ret.text());
+                if (res.error)
+                {
+                    cCard.classList.add("recipe-item-error");
+                }
+                else
+                {                       
+                    cCard.classList.remove("recipe-item-error");
+                    props.item.name = obj.name;
+                    props.item.description = obj.description;
+                    props.update();
+                    setIsEditing(false);
+                }
+            });
     };
 
     const addToFavorites = async (e) => {
         setIsFavorite(true);
+        var cCard = card;
+        cCard.classList.remove("recipe-item-error");
 
+        let obj = {
+            user_id:Constant.user_id,
+            name:props.item.name,
+            description:props.item.description,
+            url:props.item.url,
+            image_url:props.item.image_url
+        };
+
+        let js = JSON.stringify(obj);
+        await fetch(`${config.URL}/api/add-recipe`,
+        {method:'POST',body:js,headers:{'Content-Type': 'application/json', 'authorization': Constant.token}}).then(async ret => {
+            let res = JSON.parse(await ret.text());
+            if (res.error)
+            {
+                cCard.classList.add("recipe-item-error");
+                setIsFavorite(false);
+            }
+            else
+            {               
+                props.item._id = res.id        
+                cCard.classList.remove("recipe-item-error");
+            }
+        });
     };
 
-    const removeFromFavorites = (e) => {
+    const removeFromFavorites = async (e) => {
         setIsFavorite(false);
+        var cCard = card;
+        cCard.classList.remove("recipe-item-error");
+        if (!props.item._id)
+            return;
+
+        let obj = {
+            id:props.item._id,
+        };
+
+        let js = JSON.stringify(obj);
+        await fetch(`${config.URL}/api/remove-recipe`,
+        {method:'POST',body:js,headers:{'Content-Type': 'application/json', 'authorization': Constant.token}}).then(async ret => {
+            let res = JSON.parse(await ret.text());
+            if (res.error)
+            {
+                cCard.classList.add("recipe-item-error");
+                setIsFavorite(true);
+            }
+            else
+            {                       
+                cCard.classList.remove("recipe-item-error");
+            }
+        });
     };
 
     return(
-        <div className="recipe-card">
-            <div className="recipe-image"></div>
+        <div ref={(c) => card = c} className="recipe-card">
+            <div className="recipe-image">
+                <img src={props.item.image_url} alt={sName} />
+            </div>
             <div className="recipe-content">
                 <div className="recipe-header">
                     <div className="recipe-title-content">
                         {!isEditing ?
                             (<div className="recipe-title">{sName}</div>) :
-                            (<textarea className="recipe-title-edit" ref={(c) => name = c} value="Dummy Edit Text" />)
+                            (<textarea className="recipe-title-edit" ref={(c) => name = c} />)
                         }
                     </div>
                     <div className="recipe-btn-container">
@@ -59,7 +170,7 @@ const RecipeItem = (props) =>
                         <div className="recipe-btn-container">
                             {!isEditing ?
                             (<div className="recipe-button-content">
-                                <div onClick={() => setIsEditing(true)} className="recipe-btn recipe-edit">
+                                <div onClick={() => showEditor()} className="recipe-btn recipe-edit">
                                 <FontAwesomeIcon icon={solid("pencil")} />
                                 </div>
                                 <div onClick={(e) => removeItem(e)} className="recipe-btn recipe-remove">
@@ -89,7 +200,7 @@ const RecipeItem = (props) =>
                 </div>
                 <div className="recipe-desc-content">
                     {!isEditing ?
-                        (<div className="recipe-description">{sDesc}</div>) :
+                        (<div className="recipe-description">{sDesc} <a rel="noreferrer" target="_blank" href={props.item.url}>Read more</a></div>) :
                         (<textarea className="recipe-desc-edit" ref={(c) => description = c} />)
                     }
                 </div>
--- frontend/src/pages/SearchPage.js
+++ frontend/src/pages/SearchPage.js
@@ -2,9 +2,65 @@ import '../styles/SearchPage.css';
 import RecipeList from '../components/RecipeList';
 import GroceryList from '../components/GroceryList';
 import Navigation from '../components/Navigation';
+import { useState } from 'react';
+import { Constant, config } from '../Constants';
 
 const SearchPage = (props) =>
 {
+    const [ingredients, setIngredients] = useState(["sauce"]);
+    const [error, setError] = useState('');
+    const [results, setResults] = useState([]);
+    const [recipeError,setRecipeError] = useState('');
+
+    const getSearchResults = async () => {
+        setError('');
+        setRecipeError('');
+        let list = "";
+        ingredients.forEach(x => {
+            list += x + "%2C";
+        });
+        let idx = list.lastIndexOf("%2C");
+        if (idx > -1)
+            list = list.substring(0, idx);
+
+        if (list) {    
+            let obj = {
+                items:list,
+            };
+    
+            let js = JSON.stringify(obj);
+            await fetch(`${config.URL}/api/search-recipes`,
+            {method:'POST',body:js,headers:{'Content-Type': 'application/json', 'authorization': Constant.token}}).then(async ret => {
+                let res = JSON.parse(await ret.text());
+                if (res.error)
+                {
+                    setError(res.error);
+                }
+                else
+                {   
+                    if (res.result.length > 0) {
+                        let list = [];
+                        for (let i = 0; i < res.result.length; i++) {
+                            list.push({
+                                name: res.result[i].title,
+                                image_url: res.result[i].image,
+                                description: res.result[i].summary,
+                                url: res.result[i].source_url
+                            });
+                        }
+                        setResults(list);
+                    } 
+                    else {
+                        setRecipeError("no_results")
+                    }                
+                    setError('');
+                }
+            });
+        }
+        else {
+            setError("Please add items before searching");
+        }
+    }
 
     return(
         <div className="main-page-container">
@@ -12,10 +68,10 @@ const SearchPage = (props) =>
             
             <div className="main-content-container">
                 <div className="grocery-list">
-                    <GroceryList mode={"search"} />
+                    <GroceryList error={error} search={() => getSearchResults()} mode={"search"} />
                 </div>
                 <div className="recipe-list">
-                    <RecipeList searchPlaceHolder={"Search Recipes"} title={"Recipe Results"} mode={"search"} />
+                    <RecipeList setRecipeError={setRecipeError} recipeError={recipeError} results={results} searchPlaceHolder={"Search Recipes"} title={"Recipe Results"} mode={"search"} />
                 </div>
             </div>
         </div>
