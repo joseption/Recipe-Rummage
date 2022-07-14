e107a389323156a8a0126eb046af60033aa68de9
joseption <40270262+joseption@users.noreply.github.com>
2022-07-14 17:10:38 -0400

    test

--- server.js
+++ server.js
@@ -691,11 +691,11 @@ app.listen(PORT, () =>
 if (process.env.NODE_ENV === 'production') 
 {
   // Set static folder
-  app.use(express.static('frontend/build'));
+  app.use(express.static('./frontend/build'));
 
-  app.get('*', (req, res) => 
+  app.get('/*', (req, res) => 
  {
-    res.sendFile('./frontend/build/index.html')
+    res.sendFile('./frontend/build/index.html');
     //res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   });
 }
